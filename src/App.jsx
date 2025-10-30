05d223e9a8d30c4489216aee910f673646bbc573
stimglb <137829964+StimGlb@users.noreply.github.com>
2025-10-30 19:09:58 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-import GraphGenerator from './components/graph-generator.jsx'
+import { useState } from 'react'
+import Sidebar from './components/Sidebar'
+import Navbar from './components/Navbar'
+import GraphGenerator from './components/GraphGenerator'
 
 export default function App() {
-  return <GraphGenerator />
+  const [darkMode, setDarkMode] = useState(true)
+
+  return (
+    <div className={darkMode ? 'dark' : ''}>
+      <div className="flex min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-500">
+        <Sidebar />
+        <div className="flex-1 flex flex-col">
+          <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+          <main className="flex-1 p-6">
+            <GraphGenerator />
+          </main>
+        </div>
+      </div>
+    </div>
+  )
 }
